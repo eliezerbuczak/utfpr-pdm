2db18e96a97dd68aeff5095edaef0981bc75d51b
Eliezer Boeira <eliezerbuczak2016@gmail.com>
2024-04-10 21:50:18 -0300

    atv-04 - Styles e Layouts

---
+++ pdm-lesson-styles-layouts-pdmagalu/app/home.tsx
@@ -0,0 +1,20 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Scrollable from "@/components/containers/Scrollable";
+import HeaderWithTitle from "@/components/headers/HeaderWithTitle";
+import HeaderWithSearchBar from "@/components/headers/HeaderWithSearchBar";
+import AddressBar from "@/components/ui/AddressBar";
+import CardOptions from "@/components/containers/CardOptions";
+
+export default function home() {
+  return (
+    <Scrollable>
+      <HeaderWithSearchBar headerBackVisible={true} />
+
+      <AddressBar />
+        <CardOptions title={"Tem tudo no Magalu ;)"}>
+        </CardOptions>
+
+    </Scrollable>
+  );
+}
